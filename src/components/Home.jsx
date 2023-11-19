f74e22f1e2e1ac84f155b045fbf546d0518b8fcc
Ayush Thakur <ayushthakur1412@gmail.com>
2023-11-19 23:05:45 +0530

    Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Home.css';
+import backgroundImage from '../assets/pinnacle.png';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const containerStyles = {
+    position: 'relative',
+    height: '100vh', // Set the height of your component
+  };
 
-export default Home
+  const backgroundStyles = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100%',
+  };
+
+  const overlayStyles = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)', // Adjust the alpha value for the desired shade
+  };
+
+  return (
+    <>
+      <div className="home" style={containerStyles}>
+        <div style={backgroundStyles}></div>
+        <div style={overlayStyles}></div>
+        <div className="content">
+          {/* Your content goes here */}
+          Home
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
