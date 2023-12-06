32f85fef0d9316bec0c3d634ea6f163e68be5dc4
Ayush Thakur <ayushthakur1412@gmail.com>
2023-12-06 13:50:46 +0530

    Updated

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
-import stu_1 from '../assets/images/stu_1.jpg'
+import React from 'react';
+import '../css/About.css';
+import stu_1 from '../assets/images/stu_1.jpg';
+import 'boxicons';
 
 const About = () => {
   return (
-    <>
-    <div className="about_page">
-        <div className="aboutz-heading">
-            <h1>About Us</h1>
-        </div>
-        <div className="about_image">
-            <img src={stu_1} alt="student" style={{
-                width: "50%",
-                height: "auto"
-            }}/>
-        </div>
-        <div className="about_text">
-            
-        </div>
-    </div>
-    </>
-  )
-}
+    <div id='about_page' className="about_page">
+      <div className="about_heading">
+        <h1>About Us</h1>
+      </div>
 
-export default About
+      {/* Display Image and Text */}
+      <div className="about_box">
+        <div className="about_container">
+          <div className="about_image">
+            <img src={stu_1} alt="student" />
+          </div>
+
+          <div className="about_text">
+            <h1>The Pinnacle Overseas</h1>
+            <p>
+              Welcome to Pinnacle Overseas, where the convergence of language mastery and exam triumph awaits you! We are committed to taking your English proficiency to unprecedented heights, providing a transformative learning experience that seamlessly blends fluency and precision. Emphasizing both language skills and strategic exam techniques, we will lead you through the journey of mastering English for the IELTS exam. Our dedicated team of experienced instructors is enthusiastic about nurturing your abilities and ensuring you are well-prepared to overcome the challenges presented by the IELTS test. Join us on this thrilling adventure, where your aspirations for academic and professional success are not mere dreams but attainable realities.
+            </p>
+
+            <div className="about_socials" style={{ textAlign: 'center', marginTop: '20px' }}>
+              <a target='_blank' href="https://www.instagram.com/" style={{ margin: '10px', fontSize: '24px' }}>
+                <box-icon type='logo' name='instagram-alt'></box-icon>
+              </a>
+
+              <a target='_blank' href="https://www.facebook.com/" style={{ margin: '10px', fontSize: '24px' }}>
+                <box-icon type='logo' name='facebook-circle'></box-icon>
+              </a>
+
+              <a target='_blank' href="https://www.youtube.com/" style={{ margin: '10px', fontSize: '24px' }}>
+                <box-icon type='logo' name='youtube'></box-icon>
+              </a>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import GetStarted from '../Elements/GetStarted'
-import '../css/Homepage.css'
+import React from 'react';
+import GetStarted from '../Elements/GetStarted';
+import '../css/Homepage.css';
 
 const HomePage = () => {
+
+  const scrollDown = () => {
+    const aboutSection = document.getElementById('about_page');
+  
+    if (aboutSection) {
+      const navBarHeight = document.querySelector('nav').offsetHeight;
+      window.scrollTo({
+        top: aboutSection.offsetTop - navBarHeight,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
     <div className="home_page">
       <div className="home_heading">
         <h1>THE PINNACLE OVERSEAS</h1>
-        <p>This is a paragraph hvaucbjh ad abinn da jbsz  
-          skjvbjsjkbv k svkjbjsbvn kjSBDVisndvkjs skB 
-          kbasicbbsdbk s dkjbjsbd jks knkjbschibvdj s
+        <p>
+          This is a paragraph hvaucbjh ad abinn da jbsz skjvbjsjkbv k svkjbjsbvn kjSBDVisndvkjs
+          skB kbasicbbsdbk s dkjbjsbd jks knkjbschibvdj s
         </p>
       </div>
 
-      <div className="home_explore_btn">
+      <div className="home_explore_btn" onClick={scrollDown}>
         <GetStarted />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
