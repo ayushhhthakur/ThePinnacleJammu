59262a8fb95eafc01ee947d7ece93aa022f723db
Ayush Thakur <100257355+ayushhhthakur@users.noreply.github.com>
2023-12-06 21:49:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './Pages/Home';
 import About from './Pages/About';
+import Services from './Pages/Services';
+import Gallery from './Pages/Gallery';
 
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/gallery" element={<Gallery />} />
             </Routes>
           </div>
         </div>
